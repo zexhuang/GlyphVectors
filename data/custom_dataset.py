f950147d01b791754021dd4cb5c934ea27e878bd
Ubuntu <ubuntu@mrc-v100-ubuntu-20.04-beta-2.novalocal>
2022-04-03 16:42:01 +1000

    add baseline models

--- data/custom_dataset.py
+++ data/custom_dataset.py
@@ -2,27 +2,29 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-class ArchaeDataset(Dataset):
-    def __init__(self, root_dir, fixed_tensor=False, transform=None):
+class PolygonDataset(Dataset):
+    def __init__(self, root_dir, dataset, fixed_tensor=False, transform=None):
         super(Dataset).__init__()
         self.root_dir = root_dir
-        self.fixed_tensor = fixed_tensor
         self.transform = transform
         self.sample_dict = np.load(self.root_dir, allow_pickle=True)
         
-        if self.fixed_tensor:
-            self.geoms = torch.tensor(self.sample_dict['fixed_size_geoms'])
+        if fixed_tensor:
+            self.geoms = self.sample_dict['fixed_size_geoms']
         else:
-            self.geoms = torch.tensor(self.sample_dict['geoms'])
-             
-        self.feature_type = torch.tensor(self.sample_dict['feature_type'])
+            self.geoms = self.sample_dict['geoms']
+        
+        if dataset == 'archae':
+            self.feature_type = self.sample_dict['feature_type']
+        else:
+            self.feature_type = self.sample_dict['building_type']
             
     def __len__(self):
-        return len(self.sample_dict['feature_type'])
+        return len(self.feature_type)
     
     def __getitem__(self, idx):
-        geoms = self.geoms[idx].float().transpose(0, -1)
-        feature_type = self.feature_type[idx] 
+        geoms = torch.from_numpy(self.geoms[idx]).T
+        feature_type = torch.tensor(self.feature_type[idx])
         
         if self.transform:
             pass
--- trainval.py
+++ trainval.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 
-from data.custom_dataset import ArchaeDataset
+from data.custom_dataset import PolygonDataset
 from config.config_loader import load_config
 from model.net import CNN
 from utils.metric import Metrics
@@ -10,13 +10,14 @@ from utils.pytorchtools import EarlyStopping
 config = load_config()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+
 def build_model(nn, load_state=False, name=None):
     model = nn
     epoch = 1
     monitor = None
     
     if load_state:
-        checkpoint=torch.load(config['save'] + name)
+        checkpoint=torch.load(config['save'] + f'{name}.pth')
         model.load_state_dict(checkpoint['model_state'])
         monitor = checkpoint['monitor']
         epoch = checkpoint['epoch']
@@ -25,7 +26,7 @@ def build_model(nn, load_state=False, name=None):
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=config['lr'])
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 
-                                                           mode='max',
+                                                           mode='min',
                                                            factor=config['gamma'],
                                                            patience=config['step'],
                                                            verbose=True)
@@ -38,14 +39,14 @@ def train(loader, model, criterion, optimizer):
     for idx, data in enumerate(loader):
         optimizer.zero_grad()         # Clear gradients
 
-        x = data['geoms'].to(torch.device(device))
+        x = data['geoms'].to(torch.device(device), dtype=torch.float)
         y = data['feature_type'].to(torch.device(device))
         
         logits = model(x)             # Feedforward
         loss = criterion(logits, y)   # Compute gradients
 
         loss.backward()               # Backward pass
-        optimizer.step()              # Update model parametersss
+        optimizer.step()              # Update model parameters
         metr.update_cm(logits, y)     # Update confusion matrix
 
         print(f"Batch: {idx + 1}/{len(loader)}:\n"
@@ -59,7 +60,7 @@ def validation(loader, model, criterion):
     metr = Metrics(config['out_channels'])
     model.eval()
     for idx, data in enumerate(loader):
-        x = data['geoms'].to(torch.device(device))
+        x = data['geoms'].to(torch.device(device), dtype=torch.float)
         y = data['feature_type'].to(torch.device(device))
 
         logits = model(x)
@@ -75,33 +76,50 @@ def validation(loader, model, criterion):
 
 
 if __name__ == "__main__": 
-    EPOCH = config['epoch']
-    # Dataset
-    training_data = ArchaeDataset(root_dir=config['data_path']
-                                  + config['dataset']['archae']['train_file'], 
-                                  fixed_tensor=True)
-    test_data = ArchaeDataset(root_dir=config['data_path']
-                              + config['dataset']['archae']['test_file'], 
-                              fixed_tensor=True)
-
-    train_loader = DataLoader(training_data, batch_size=config['batch_size'], shuffle=True)
-    test_loader = DataLoader(test_data, batch_size=config['batch_size'], shuffle=True)
+    # dataset
+    dataset = config['dataset']
+    training_data = PolygonDataset(root_dir=config['data_path']
+                                   +config[dataset]['train'], 
+                                   dataset=dataset,
+                                   fixed_tensor=True)
+    test_data = PolygonDataset(root_dir=config['data_path']
+                               +config[dataset]['test'], 
+                               dataset=dataset,
+                               fixed_tensor=True)
         
-    model, criterion, optimizer, scheduler, monitor, epoch = build_model(CNN(in_channels=config['in_channels'], 
-                                                                         out_channels=config['out_channels']))
+    train_loader = DataLoader(training_data, 
+                              batch_size=config['batch_size'], 
+                              shuffle=True)
+    test_loader = DataLoader(test_data, 
+                             batch_size=config['batch_size'], 
+                             shuffle=True)
+    # build model
+    model, criterion, \
+    optimizer, scheduler, \
+    monitor, epoch = build_model(CNN(in_channels=config['in_channels'], 
+                                     out_channels=config['out_channels']),
+                                     config['load_state'],
+                                     config['checkpoint'])
     
-    early_stopping = EarlyStopping(path=config['save']+'baseline.pth', 
+    early_stopping = EarlyStopping(path=config['save']
+                                   +f'{config["checkpoint"]}.pth', 
                                    best_score=monitor,
                                    patience=config['patience'], 
                                    verbose=True)
-
+    # training loop
+    EPOCH = config['epoch']
     for epoch in range(1, EPOCH+1):
-        # Training & Validation
-        train(train_loader, model, criterion, optimizer)
-        metr, loss = validation(test_loader, model, criterion)
-        scheduler.step(loss)
-        # Early-stopping    
-        early_stopping(loss, metr.cm, model, optimizer, epoch)
-        if early_stopping.early_stop:
-            print("Early stopping")
+        if config['train']:
+            print(f'At Epoch [{epoch}/{EPOCH}]:')
+            # Training & Validation
+            train(train_loader, model, criterion, optimizer)
+            metr, loss = validation(test_loader, model, criterion)
+            scheduler.step(loss)
+            # Early-stopping    
+            early_stopping(loss, metr.cm, model, optimizer, epoch)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break 
+        else:
+            validation(test_loader, model, criterion)
             break
